7be7392a5366fc1ffb297031e470ed7394ab1b56
Arul <arul.maulana_ti22@nusaputra.ac.id>
2025-08-11 10:29:12 +0700

    perubahan semua

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,78 @@
 import React from 'react';
-import './styles/Footer.css';
+import { Link } from 'react-router-dom';
+import '../components/styles/Footer.css';
 
-// Impor logo bank dari direktori assets
-import bcaLogo from '../assets/images/bca.png';
-import bniLogo from '../assets/images/bni.png';
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>Copyright © 2025 PT. Bina Barokah Sejahtera</p>
-      <div className="bank-logos">
-        <img src={bcaLogo} alt="Logo Bank BCA" />
-        <img src={bniLogo} alt="Logo Bank BNI" />
-      </div>
-    </footer>
-  );
-}
+
+import mandiri from '../assets/images/mandiri.png';
+import bca from '../assets/images/bca.png';
+import bni from '../assets/images/bni.png';
+import asita from '../assets/images/asita.png';
+import won from '../assets/images/won.png';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="main-footer">
+                <div className="footer-container">
+                    <div className="footer-col">
+                        <h3>PT. Bina Barokah Sejahtera</h3>
+                        <p>
+                            Selamat Datang di PT. Bina Barokah Tour – Awali Perjalanan Istimewa Anda Bersama Kami!<br /><br />
+                            Mari jelajahi keindahan dunia dengan tenang dan nyaman – karena setiap langkah adalah berkah dan kenangan tak ternilai.
+                        </p>
+                    </div>
+                    <div className="footer-col">
+                        <h3>Kantor Pusat</h3>
+                        <ul>
+                            <li>🏢 GEDUNG GRAHA BAROKAH</li>
+                            <li>📍 Jl. Raya Cisaat No. 1 Sukamanah Cisaat – Sukabumi</li>
+                            <li>📧 <a href="mailto:barokahtour.travel@gmail.com">barokahtour.travel@gmail.com</a></li>
+                            <li>📞
+                                <a href="https://wa.me/62266230408" target="_blank" rel="noopener noreferrer">0266 230 408</a> /
+                                <a href="https://wa.me/6285930005544" target="_blank" rel="noopener noreferrer">0859 3000 5544</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <section className="lokasi">
+                            <h3>Lokasi Kami</h3>
+                            <div className="map-container">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.8413543183374!2d106.89146117356522!3d-6.9095652676201915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68482b702b6599%3A0xfd5d3d335f6d8bd8!2sBarokah%20Tour%20%26%20Travel!5e0!3m2!1sid!2sid!4v1752729967693!5m2!1sid!2sid"
+                                    width="100%"
+                                    height="300"
+                                    style={{ border: 0 }}
+                                    allowFullScreen=""
+                                    loading="lazy"
+                                    referrerPolicy="no-referrer-when-downgrade"
+                                ></iframe>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-bar">
+                <div className="footer-content-bar">
+                    <div className="footer-logos">
+                        <img src={mandiri} alt="Mandiri" />
+                        <img src={bca} alt="BCA" />
+                        <img src={bni} alt="BNI" />
+                        <img src={asita} alt="Kemenag RI" />
+                        <img src={won} alt="Wonderful Indonesia" />
+                    </div>
+                    <div className="footer-text">
+                        <p>Copyright © 2025 PT. Bina Barokah Sejahtera</p>
+                        <div className="social-icons">
+                            <a href="https://www.instagram.com/barokahtourindonesia_/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+                            <a href="https://www.tiktok.com/@barokahtour_?is_from_webapp=1&sender_device=pc" target="_blank" rel="noopener noreferrer"><i className="fab fa-tiktok"></i></a>
+                            <a href="https://youtube.com/@barokahtoursmi?si=-yRfaFEfKaC-lelk" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
